816babf396739a69e3cbcd04d0bfaec917b77000
AleDias <70322758+AleDiass@users.noreply.github.com>
2020-12-03 21:17:18 -0300

    Add files via upload

--- src/Services/Api.js
+++ src/Services/Api.js
@@ -5,8 +5,9 @@ async function getData(){
     if(!version){
         const responseVersion =await fetch('https://ddragon.leagueoflegends.com/api/versions.json')
         version = await responseVersion.json();
-        
+        window.localStorage.setItem('@_Version__',version[1])    
     }
+
 
     const {data} = await fetch(`https://ddragon.leagueoflegends.com/cdn/10.24.1/data/en_US/champion.json`)
                     .then(res => res.json())
@@ -20,7 +21,9 @@ async function getData(){
 }
 
 async function getChampionInfo(champname,lang='en_US'){
-const response = await fetch(`https://ddragon.leagueoflegends.com/cdn/${version[1] || '10.24.1'}/data/${lang}/champion/${champname}.json`)
+
+const versionLocalStorage =await window.localStorage.getItem('@_Version__')
+const response = await fetch(`https://ddragon.leagueoflegends.com/cdn/${versionLocalStorage || '10.24.1'}/data/${lang}/champion/${champname}.json`)
 const {data} = await response.json();
 
 
